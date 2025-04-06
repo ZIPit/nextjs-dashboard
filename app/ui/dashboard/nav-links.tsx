543c35dc301a50b1e60bb869f889f135d83e6160
Ivan Zubenko <ivzubenko@gmail.com>
2025-04-06 14:26:03 +0300

    3 commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -19,7 +19,6 @@ const links = [
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
-///asdfafasf
 export default function NavLinks() {
   const pathname = usePathname();
   return (
